bdda6bcdb3af575bec87747eb273cfcb2c76925a
Brandon Webb <Brandon.Webb@vistaprint.com>
2024-08-27 12:50:53 -0400

    chore: updating all dx and preparing to switch to nextjjs

---
+++ eslint.config.js
@@ -0,0 +1,5 @@
+import eslint from '@thesouschef/eslint-config';
+import react from '@thesouschef/eslint-config-react';
+import next from '@thesouschef/eslint-config-next';
+
+export default [...eslint, ...react, ...next];
